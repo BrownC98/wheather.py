c6d4b03a657e6aa46d5d4884c9bc4ef171c624a9
donggun_Kal <0429donggun@gmail.com>
2020-08-23 12:18:06 +0900

    입력 수정

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from properties import exel_file_name
 from weather import *
 from openpyxl import load_workbook
 
-province = '고운동'
+province = input('원하는 동네 이름을 입력하세요')
 
 if __name__ == "__main__":
     request_and_print(province)
